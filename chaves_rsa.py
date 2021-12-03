a4beea6c147313d70a4e325c5879f4c4bc5149ec
Marcelo Custódio <custodios.marcelo@gmail.com>
2021-12-03 11:23:02 -0300

    Commit inicial, env criado

---
+++ chaves_rsa.py
@@ -0,0 +1,18 @@
+from cryptography.hazmat import primitives
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=512)
+pem_private = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption())
+key_private = pem_private
+print(key_private)
+
+print()
+
+public_key = private_key.public_key()
+pem_public = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.PKCS1)
+key_public = pem_public
+print(key_public)
